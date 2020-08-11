9997f56ab78a1ebffca1a21d0348351f764b892e
tomomi.koyama <smalllamp.t.k@gmail.com>
2020-08-11 16:14:47 +0900

    pod installと.gitignoreの作成まで実施

---
+++ TaskManagement/InputViewController.swift
@@ -0,0 +1,30 @@
+//
+//  InputViewController.swift
+//  TaskManagement
+//
+//  Created by 小山燈実 on 2020/08/10.
+//  Copyright © 2020 tomomi.koyama. All rights reserved.
+//
+
+import UIKit
+
+class InputViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
