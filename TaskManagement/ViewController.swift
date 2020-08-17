0e49d4e96a0fe08f89f7af2d6ef5a9cc8f1a02e6
tomomi.koyama <smalllamp.t.k@gmail.com>
2020-08-17 18:11:47 +0900

    categoryというStringプロパティを追加し、入力ができる様にしました。searchbarを設置し、カテゴリーに絞って検索できる様にしました。

--- TaskManagement/ViewController.swift
+++ TaskManagement/ViewController.swift
@@ -7,27 +7,58 @@
 //
 
 import UIKit
+import RealmSwift
+import UserNotifications
 
-class ViewController: UIViewController , UITableViewDelegate, UITableViewDataSource {
-    @IBOutlet weak var tabelView: UITableView!
+class ViewController: UIViewController , UITableViewDelegate, UITableViewDataSource, UISearchBarDelegate {
+    @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var searchBar: UISearchBar!
+    
+    
+    let realm = try! Realm()
+    
+    var taskArray = try! Realm().objects(Task.self).sorted(byKeyPath: "date", ascending: true)
+    
+    
+    // モデルクラスを使用し、取得データを格納する変数を作成を試してみました。
+    var seachResults: [String] = []
+    var tableCells: Results<Task>!
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        tabelView.delegate = self
-        tabelView.dataSource = self
+        
+        tableView.delegate = self
+        tableView.dataSource = self
+        
+        searchBar.delegate = self
+        searchBar.enablesReturnKeyAutomatically = true
+        searchBar.showsCancelButton = true
+        
+        // データ全権取得(https://www.casleyconsulting.co.jp/blog/engineer/4579/)
+        self.tableCells = realm.objects(Task.self)
+        
     }
     
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 0
+        return taskArray.count
     }
     
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         
         let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        
+        let task = taskArray[indexPath.row]
+        cell.textLabel?.text = task.title
+        
+        let formatter = DateFormatter()
+        formatter.dateFormat = "yyyy-MM-dd HH:mm"
+        
+        let dateString:String = formatter.string(from: task.date)
+        cell.detailTextLabel?.text = dateString
         
         return cell
     }
@@ -44,10 +75,70 @@ class ViewController: UIViewController , UITableViewDelegate, UITableViewDataSou
     
     
     func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        
+        
+        if editingStyle == .delete {
+            
+            let task = self.taskArray[indexPath.row]
+            
+            
+            let center = UNUserNotificationCenter.current()
+            center.removePendingNotificationRequests(withIdentifiers: [String(task.id)])
+            
+            
+            try! Realm().write {
+                self.realm.delete(self.taskArray[indexPath.row])
+                tableView.deleteRows(at: [indexPath], with: .fade)
+            }
+            
+            center.getPendingNotificationRequests { (requests: [UNNotificationRequest]) in
+                for request in requests {
+                    print("/---------------")
+                    print(request)
+                    print("---------------/")
+                }
+            }
+        }
+        
     }
     
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?){
+        let inputViewController:InputViewController = segue.destination as! InputViewController
+        
+        if segue.identifier == "cellSegue" {
+            let indexPath = self.tableView.indexPathForSelectedRow
+            inputViewController.task = taskArray[indexPath!.row]
+        } else {
+            let task = Task()
+            
+            let allTasks = realm.objects(Task.self)
+            if allTasks.count != 0 {
+                task.id = allTasks.max(ofProperty: "id")! + 1
+            }
+            
+            inputViewController.task = task
+        }
+    }
     
+    //   func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {}を試しました。
+    func searchBarSearchButtonClicked(_ seachBar: UISearchBar) {
+        
+        let predicate = NSPredicate(format: "category == %@", seachBar.text!)
+        taskArray = realm.objects(Task.self).filter(predicate)
+        tableView.reloadData()
+        
+    }
     
+    //   searchBar.showsCancelButton = falseを試しました。
+    func searchBarCancelButtonClicked(_ seachBar: UISearchBar) {
+        searchBar.text = ""
+        taskArray = try!Realm().objects(Task.self).sorted(byKeyPath:"date",ascending:true)
+        tableView.reloadData()
+    }
     
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        tableView.reloadData()
+    }
 }
 
